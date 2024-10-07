d31d222d1d0f04709164b3d3aa9c95dd22a936b0
enigmaXploit <pencadangangogel@gmail.com>
2024-10-07 18:20:33 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+apt update && apt upgrade
+pkg install nala -y
+pkg install bash curl -y
+pkg install mpv -y
+pkg install jq -y
+pkg install ruby nano -y
+pkg install lolcat
+gem install lolcat
+git clone https://github.com/EnigmaXploit/TOOLSDARKV2.sh.git
+cd TOOLSDARKV2.sh
+chmod × 777
+bash TOOLSDARKV2.sh
